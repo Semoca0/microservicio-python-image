010f31c45a5af319a7bcc93c0c843bfcc130c237
DanielAnto <danielanto2001@gmail.com>
2023-05-30 17:25:03 -0500

    second commit

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def upload_file():
             return 'No allowed extension'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
